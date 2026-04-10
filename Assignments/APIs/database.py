256c06c3cbe179eeb63c6b4046f030449b1d9f5a
siddarthakaleru-p99 <siddartha.kaleru@p99soft.com>
2026-04-10 10:23:43 +0530

    Delete Assignments directory

--- Assignments/APIs/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://postgres:Bannu%40123@localhost:5432/fastapi_db"
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
